504c44c8321c766483a0e69a8f30e75199b13edb
Ekaterina Ploskonosova <ploskev@yahoo.com>
2022-11-14 13:09:04 +0500

    commit

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import Bowman from './bowman.js';
+import Daemon from './daemon.js';
+import Magician from './magician.js';
+import Swordsman from './swordsman.js';
+import Undead from './undead.js';
+import Zombie from './zombie.js';
+
+class Game {
+  constructor() {
+    this.chars = [];
+    this.chars.push(
+      new Bowman('bowman'),
+      new Daemon('daemon'),
+      new Magician('magician'),
+      new Swordsman('swordsman'),
+      new Undead('undead'),
+      new Zombie('zombie'),
+    );
+  }
+}
+
+const game = new Game();
+console.log(game);
